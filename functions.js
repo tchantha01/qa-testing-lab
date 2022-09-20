dbd240017b607e747cd68a78717cda672377e007
Toky Chanthavong <tchantha01@gmail.com>
2022-09-19 21:42:04 -0600

    qa testing lab

--- functions.js
+++ functions.js
@@ -13,7 +13,14 @@ module.exports = {
     },
 
     subtract: (num1, num2) => {
-        return num1 - num2
+        let result = 0
+        for (let i = 0; i < arguments.length; i++) {
+            if (arguments.length > 2)
+            console.log('Error here')
+        } else {
+            result = num1 - num2
+        }
+        return result
     },
 
     multiply: (num1, num2) => {
